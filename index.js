a6ecea7d98fc8e8605b3017a687f517bdcb53096
Josh <cheukhinjoshua.ng@gmail.com>
2019-05-30 03:15:42 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,9 +10,9 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
 }
 
 function deleteFromObjectByKey(object, key){
-  var clone = object.assign({},object);
+  var clone = Object.assign({}, object);
   delete clone[key];
-  return clone
+  return clone;
 }
 
 function destructivelyDeleteFromObjectByKey(object,key){
